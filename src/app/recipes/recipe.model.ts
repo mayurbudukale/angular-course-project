b0e8a1b92373c31a46c665051517c602b52340a8
Mayur Budukale <mayurbudukale@gmail.com>
2018-12-01 14:28:06 +0530

    first commit

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,15 @@
+export class Recipe {
+  // public name: string;
+  // public description: string;
+  // public imagePath: string;
+  // You can always just declare the constructor arguments as public
+  constructor(
+    public name: string,
+    public description: string,
+    public imagePath: string
+  ) {
+    // this.name = name;
+    // this.description = desc;
+    // this.imagePath = imagePath;
+  }
+}
